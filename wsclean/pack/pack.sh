19717f27f90e74d2f2d12898abf0c87e1165e2d8
André Offringa <offringa@gmail.com>
2015-04-09 16:01:14 +0200

    Adding interface files to packing script

--- wsclean/pack/pack.sh
+++ wsclean/pack/pack.sh
@@ -8,19 +8,23 @@ else
 		mkdir /tmp/wsclean/aocommon
 		mkdir /tmp/wsclean/beam
 		mkdir /tmp/wsclean/cleanalgorithms
+		mkdir /tmp/wsclean/interface
 		mkdir /tmp/wsclean/lofar
 		mkdir /tmp/wsclean/model
 		mkdir /tmp/wsclean/msproviders
 		mkdir /tmp/wsclean/parser
+		mkdir /tmp/wsclean/pywsclean
 		cd ..
 		cp -v CMakeLists.txt angle.h areaset.* banddata.* beamevaluator.* buffered_lane.* cachedimageset.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* layeredimager.* multibanddata.* nlplfitter.* matrix2x2.* modelrenderer.* msselection.* polarizationenum.* progressbar.* radeccoord.* sourcesdf.* sourcesdfwithsamples.* spectralenergydistribution.* stopwatch.* tilebeam.* uvector.* uvwdistribution.* weightmode.* wsclean.* wscleanmain.cpp wsinversion.* wscversion.h /tmp/wsclean/
 		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
 		cp -v beam/*.{h,cpp} /tmp/wsclean/beam
 		cp -v cleanalgorithms/*.{h,cpp} /tmp/wsclean/cleanalgorithms/
+		cp -v interface/*.{c,h,cpp} /tmp/wsclean/interface
 		cp -v lofar/*.{h,cpp} /tmp/wsclean/lofar/
 		cp -v model/*.{h,cpp} /tmp/wsclean/model/
 		cp -v msproviders/*.{h,cpp} /tmp/wsclean/msproviders
 		cp -v parser/*.h /tmp/wsclean/parser
+		cp -v pywsclean/*.py /tmp/wsclean/pywsclean
 		cd /tmp
 		mv wsclean wsclean-${VERSION}
 		tar -cjvf ${WORKDIR}/wsclean-${VERSION}.tar.bz2 wsclean-${VERSION}/
