a45e35ee55b8a3e7b18e7b80c37d629b87720267
André Offringa <offringa@gmail.com>
2015-06-01 08:18:30 +0200

    Updating pack script for new files

--- wsclean/pack/pack.sh
+++ wsclean/pack/pack.sh
@@ -11,14 +11,16 @@ else
 		mkdir /tmp/wsclean/lofar
 		mkdir /tmp/wsclean/model
 		mkdir /tmp/wsclean/msproviders
+		mkdir /tmp/wsclean/wsclean
 		cd ..
-		cp -v CMakeLists.txt angle.h areaset.* banddata.* buffered_lane.* cachedimageset.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* layeredimager.* multibanddata.* nlplfitter.* matrix2x2.* modelrenderer.* msselection.* polarizationenum.* progressbar.* radeccoord.* stopwatch.* uvector.* weightmode.* wsclean.* wscleanmain.cpp wsinversion.* wscversion.h /tmp/wsclean/
+		cp -v CMakeLists.txt angle.h areaset.* banddata.* buffered_lane.* cachedimageset.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* multibanddata.* nlplfitter.* matrix2x2.* modelrenderer.* msselection.* polarizationenum.* progressbar.* radeccoord.* stopwatch.* uvector.* weightmode.* wscleanmain.cpp wscversion.h /tmp/wsclean/
 		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
 		cp -v deconvolution/*.{h,cpp} /tmp/wsclean/deconvolution/
 		cp -v interface/*.{c,h,cpp,py} /tmp/wsclean/interface
 		cp -v lofar/*.{h,cpp} /tmp/wsclean/lofar/
 		cp -v model/*.{h,cpp} /tmp/wsclean/model/
 		cp -v msproviders/*.{h,cpp} /tmp/wsclean/msproviders
+		cp -v wsclean/*.{h,cpp} /tmp/wsclean/wsclean
 		cd /tmp
 		mv wsclean wsclean-${VERSION}
 		tar -cjvf ${WORKDIR}/wsclean-${VERSION}.tar.bz2 wsclean-${VERSION}/
