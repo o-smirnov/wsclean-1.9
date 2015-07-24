4cd931c40590a71f0804fb1d8d55a464af489959
André Offringa <offringa@gmail.com>
2015-07-24 16:55:58 +0200

    Fixing pack script and adding -O3 -march=native to example

--- wsclean/pack/pack.sh
+++ wsclean/pack/pack.sh
@@ -8,19 +8,23 @@ else
 		mkdir /tmp/wsclean/aocommon
 		mkdir /tmp/wsclean/deconvolution
 		mkdir /tmp/wsclean/interface
+		mkdir /tmp/wsclean/iuwt
 		mkdir /tmp/wsclean/lofar
 		mkdir /tmp/wsclean/model
 		mkdir /tmp/wsclean/msproviders
 		mkdir /tmp/wsclean/wsclean
+		mkdir /tmp/wsclean/wsclean/examples
 		cd ..
-		cp -v CMakeLists.txt angle.h areaset.* banddata.* buffered_lane.* cachedimageset.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imagebufferallocator.* imageweights.* inversionalgorithm.* lane.* multibanddata.* nlplfitter.* matrix2x2.* modelrenderer.* msselection.* polarizationenum.* progressbar.* radeccoord.* stopwatch.* uvector.* weightmode.* wscleanmain.cpp wscversion.h /tmp/wsclean/
+		cp -v CMakeLists.txt Doxyfile.in angle.h areaset.* banddata.* buffered_lane.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fftwmultithreadenabler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imageweights.* inversionalgorithm.* lane.* multibanddata.* nlplfitter.* matrix2x2.* modelrenderer.* msselection.* polarizationenum.* progressbar.* radeccoord.* stopwatch.* threadpool.* uvector.* weightmode.* wscleanmain.cpp wscversion.h /tmp/wsclean/
 		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
 		cp -v deconvolution/*.{h,cpp} /tmp/wsclean/deconvolution/
-		cp -v interface/*.{c,h,cpp,py} /tmp/wsclean/interface
+		cp -v interface/*.{c,h,cpp,py} /tmp/wsclean/interface/
+		cp -v iuwt/*.{h,cpp} /tmp/wsclean/iuwt/
 		cp -v lofar/*.{h,cpp} /tmp/wsclean/lofar/
 		cp -v model/*.{h,cpp} /tmp/wsclean/model/
 		cp -v msproviders/*.{h,cpp} /tmp/wsclean/msproviders
 		cp -v wsclean/*.{h,cpp} /tmp/wsclean/wsclean
+		cp -v wsclean/examples/{Makefile,*.cpp} /tmp/wsclean/wsclean/examples/
 		cd /tmp
 		mv wsclean wsclean-${VERSION}
 		tar -cjvf ${WORKDIR}/wsclean-${VERSION}.tar.bz2 wsclean-${VERSION}/
@@ -30,6 +34,6 @@ else
 		mkdir build
 		cd build
 		cmake ../
-		make -j 12
+		make -j 4
 		cat ../wscversion.h
 fi
